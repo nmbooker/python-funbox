4a244ee5f5fdf6765da31dc6c8bb94ff57da3fc5
Nick Booker <NMBooker@gmail.com>
2013-09-01 17:02:14 +0100

    Release v0.3, now with a setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "funbox",
+    version = "0.3",
+    packages = find_packages(),
+    author = "Nick Booker",
+    description = "Functional Toolbox",
+    license = "MIT",
+    url = "https://github.com/nmbooker/python-funbox",
+)
