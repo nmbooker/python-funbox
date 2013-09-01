68775b6bfd133a7383504c77d2b74d23cfcda7af
Nick Booker <NMBooker@gmail.com>
2013-09-01 17:11:37 +0100

    Added lists.uncons() function.

--- funbox/lists.py
+++ funbox/lists.py
@@ -15,6 +15,14 @@ def all_but_last(n):
         raise ValueError("n must be an integer from 0 upwards, got %r" % (n,))
     return lambda sequence : sequence[:-n]
 
+def uncons(sequence):
+    """uncons(sequence) => (sequence[0], sequence[1:])
+
+    >>> uncons([1, 2, 3, 4, 5])
+    (1, [2, 3, 4, 5])
+    """
+    return sequence[0], sequence[1:]
+
 if __name__ == "__main__":
     import doctest
     doctest.testmod()
