1f4d99d2ce3bfae0b3519e249f57c5bce9f81cc2
Nick Booker <NMBooker@gmail.com>
2013-09-02 20:24:58 +0100

    Improve Python 3 support in validation.py

--- funbox/validation.py
+++ funbox/validation.py
@@ -18,7 +18,30 @@ def is_natural_number(n):
     >>> is_natural_number(1.5)
     False
     """
-    return isinstance(n, (int, long)) and n >= 0
+    return is_int(n) and n >= 0
+
+def is_int(n):
+    """Return whether n is an int or long type
+    >>> is_int(0)
+    True
+    >>> is_int(1)
+    True
+    >>> is_int(-1)
+    True
+    >>> is_int(8474372938474373823483927843392837439)
+    True
+    >>> is_int(1.5)
+    False
+    >>> is_int('1')
+    False
+    """
+    int_types = (int,)
+    try:
+        int_types += (long,)
+    except NameError:
+        # This takes care of there not being a 'long' type in Python 3
+        pass
+    return isinstance(n, int_types)
 
 if __name__ == "__main__":
     import doctest
