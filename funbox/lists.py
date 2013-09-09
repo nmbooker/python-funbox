4d71978a0b2c98acd5117960b2d73a2950c1dde0
Nick Booker <NMBooker@gmail.com>
2013-09-09 19:32:51 +0100

    Rationalised categorise function.

--- funbox/lists.py
+++ funbox/lists.py
@@ -23,10 +23,10 @@ def uncons(sequence):
     """
     return sequence[0], sequence[1:]
 
-def categorise(functions):
-    """categorise(functions)(sequence) - Split up a sequence according to functions.
+def categorise(functions, unique=False):
+    """categorise(functions, unique=False)(sequence) - Split up a sequence according to functions.
 
-    :: [a -> bool] -> [a] -> [[a]]
+    :: ([a -> bool], bool) -> [a] -> [[a]]
 
     functions: A list of predicates, one for each list you want as output.
     sequence: The sequence you want to categorise.
@@ -40,51 +40,27 @@ def categorise(functions):
     >>> even = lambda x: x % 2 == 0
     >>> multiple_of_3 = lambda x : x % 3 == 0
     >>> nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    >>> evens, multiples_of_3, rest = categorise([even, multiple_of_3])(nums)
-    >>> evens
-    [2, 4, 6, 8, 10]
-    >>> multiples_of_3
-    [3, 6, 9]
-    >>> rest
-    [1, 5, 7]
+    >>> categorise([even, multiple_of_3])(nums)
+    [[2, 4, 6, 8, 10], [3, 6, 9], [1, 5, 7]]
+    >>> categorise([even, multiple_of_3], unique=True)(nums)
+    [[2, 4, 6, 8, 10], [3, 9], [1, 5, 7]]
+    >>> categorise([multiple_of_3, even], unique=True)(nums)
+    [[3, 6, 9], [2, 4, 8, 10], [1, 5, 7]]
     """
     def categorise_functions(sequence):
-        return _categorise_helper(functions, sequence, unique=False)
+        out_lists = [list() for i in range(len(functions) + 1)]
+        for item in sequence:
+            caught = False
+            for idx, fun in enumerate(functions):
+                if fun(item):
+                    out_lists[idx].append(item)
+                    caught = True
+                    if unique:
+                        break
+            if not caught:
+                out_lists[-1].append(item)
+        return out_lists
     return categorise_functions
-
-def categorise_unique(functions):
-    """categorise_unique(functions)(sequence)
-    like 'categorise()()' but each item can only appear in one of the output lists.
-
-    >>> even = lambda x: x % 2 == 0
-    >>> mult_of_3 = lambda x : x % 3 == 0
-    >>> nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    >>> evens, multiples_of_3, rest = categorise_unique([even, mult_of_3])(nums)
-    >>> evens
-    [2, 4, 6, 8, 10]
-    >>> multiples_of_3
-    [3, 9]
-    >>> rest
-    [1, 5, 7]
-    """
-    def categorise_unique_funs(sequence):
-        return _categorise_helper(functions, sequence, unique=True)
-    return categorise_unique_funs
-
-def _categorise_helper(functions, sequence, unique):
-    out_lists = [list() for i in range(len(functions) + 1)]
-    for item in sequence:
-        caught = False
-        for idx, fun in enumerate(functions):
-            if fun(item):
-                out_lists[idx].append(item)
-                caught = True
-                if unique:
-                    break
-        if not caught:
-            out_lists[-1].append(item)
-    return out_lists
-
 
 if __name__ == "__main__":
     import doctest
