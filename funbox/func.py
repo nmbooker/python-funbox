9502143e473c84da437d34bef524557ad0c0d25c
Nick Booker <NMBooker@gmail.com>
2014-07-13 16:44:22 +0100

    Add op and func modules.

---
+++ funbox/func.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+
+"""Functions for fiddling around with functions.
+"""
+
+def flip(func):
+    """Flip the order of arguments of a curried function.
+
+    For example, we could define lt as being equal to flip(gt):
+    >>> from op import lt, gt
+    >>> lt(3)(2) == flip(gt)(3)(2)
+    True
+    """
+    def _flipped(*args_x, **kwargs_x):
+        def _flipped_inner(*args_y, **kwargs_y):
+            return func(*args_y, **kwargs_y)(*args_x, **kwargs_x)
+        return _flipped_inner
+    return _flipped
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
---
+++ funbox/op.py
@@ -0,0 +1,178 @@
+#! /usr/bin/env python
+
+"""Curried operators.
+
+Useful for map, filter, takewhile, partition etc.
+"""
+
+from __future__ import division
+from functools import partial
+
+def lt(y):
+    """lt(y)(x) = x < y
+
+    >>> list(filter(lt(3), [1,2,3,4,5]))
+    [1, 2]
+    """
+    return lambda x : x < y
+
+def le(y):
+    """le(y)(x) = x <= y
+
+    >>> list(filter(le(3), [1,2,3,4,5]))
+    [1, 2, 3]
+    """
+    return lambda x : x <= y
+
+def gt(y):
+    """gt(y)(x) = x > y
+
+    >>> list(filter(gt(3), [1,2,3,4,5]))
+    [4, 5]
+    """
+    return lambda x : x > y
+
+def ge(y):
+    """ge(y)(x) = x > y
+
+    >>> list(filter(ge(3), [1,2,3,4,5]))
+    [3, 4, 5]
+    """
+    return lambda x : x >= y
+
+def eq(y):
+    """eq(y)(x) = x == y
+
+    >>> list(filter(eq(3), [1,2,3,4,5]))
+    [3]
+    """
+    return lambda x : x == y
+
+def ne(y):
+    """eq(y)(x) = x != y
+
+    >>> list(filter(ne(3), [1,2,3,4,5]))
+    [1, 2, 4, 5]
+    """
+    return lambda x : x != y
+
+def add(y):
+    """add(y)(x) = x + y
+
+    Be careful with types where + is not commutative.
+
+    >>> add(2)(3)
+    5
+    """
+    return lambda x : x + y
+
+def mul(y):
+    """mul(y)(x) = x * y
+
+    Be careful with types where * is not commutative.
+
+    >>> mul(2)(3)
+    6
+    >>> mul(5)('a')
+    'aaaaa'
+    """
+    return lambda x : x * y
+
+def take_away(y):
+    """take_away(y)(x) = x - y
+
+    Be careful with this because subtraction is not commutative.
+    The flipped version of this is 'take_away'
+
+    >>> list(map(take_away(2), [1,2,3,4,5]))
+    [-1, 0, 1, 2, 3]
+    """
+    return lambda x : x - y
+
+def take_away_from(y):
+    """take_away_from(y)(x) = y - x
+
+    Be careful with this because subtraction is not commutative.
+    The flipped version of this is 'take_away'
+
+    >>> list(map(take_away_from(2), [1,2,3,4,5]))
+    [1, 0, -1, -2, -3]
+    """
+    return lambda x : y - x
+
+def divide_by(y):
+    """divide_by(y)(x) = x / y
+
+    Note I'm using the future division here - the answer on two ints will
+    be a float.
+
+    Be careful with this because division is not commutative.
+
+    You may be interested in func.flip(divide_by) to reverse the arguments.
+
+    >>> divide_by(2)(4)
+    2.0
+    >>> divide_by(2)(5)
+    2.5
+    >>> from func import flip
+    >>> flip(divide_by)(5)(2)
+    2.5
+    """
+    return lambda x : x / y
+
+def intdiv_by(y):
+    """intdiv_by(y)(x) = x // y
+
+    Be careful with this because division is not commutative.
+
+    You may be interested in func.flip(intdiv_by) to reverse the arguments.
+
+    >>> intdiv_by(2)(4)
+    2
+    >>> intdiv_by(2)(5)
+    2
+    >>> from func import flip
+    >>> flip(intdiv_by)(5)(2)
+    2
+    """
+    return lambda x : x // y
+
+def divmod_by(y):
+    """divmod_by(y)(x) = divmod(x, y)
+
+    Be careful with this because division is not commutative.
+
+    You may be interested in func.flip(divmod_by) to reverse the arguments.
+
+    >>> divmod_by(2)(4)
+    (2, 0)
+    >>> divmod_by(2)(5)
+    (2, 1)
+    >>> from func import flip
+    >>> flip(divmod_by)(5)(2)
+    (2, 1)
+    """
+    return lambda x : divmod(x, y)
+
+def modulo(y):
+    """modulo(y)(x) = x % y
+
+    Be careful with this because the % operator is not commutative.
+
+    You may be interested in func.flip(modulo) to reverse the arguments.
+
+    >>> modulo(2)(4)
+    0
+    >>> modulo(2)(5)
+    1
+    >>> from func import flip
+    >>> flip(modulo)(5)(2)
+    1
+    """
+    return lambda x : x % y
+
+fmt = modulo    # % is also used for string formatting.
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
