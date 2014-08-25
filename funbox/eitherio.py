99e8ad3d445692db95f312eaab1d10b5a8134f86
Nick Booker <NMBooker@gmail.com>
2014-08-25 21:28:55 +0100

    Playing with Either-based IO generators.

--- funbox/eitherio.py
+++ funbox/eitherio.py
@@ -1,4 +1,16 @@
-"""IO for Python using Either.
+"""IO using Either.
+
+These functions enable you to write pure code to generate stdout and stderr
+streams.
+
+Why is this useful?
+
+You can write unit tests or even doctests for your data generation code,
+and separate the generation of output from how it gets output.
+
+For the latter, you could for example decide to write a function with
+the same interface as write_either that logs Left items to a logging server
+and writes Right items to a TCP socket.
 """
 
 def write_either(items):
@@ -10,3 +22,14 @@ def write_either(items):
         out_file = (sys.stdout if item.is_right() else sys.stderr)
         string = (item.value if item.is_right() else item.message)
         out_file.write(string)
+
+
+def interact_either(func):
+    """Take function mapping stdin lines to stdout and stderr chunks.
+
+    Note you must put newline characters on the end of stdout and stderr
+    strings.
+
+    Wrap strings for stdout in Right, and strings for stderr in Left.
+    """
+    write_either(func(iter(sys.stdin)))
