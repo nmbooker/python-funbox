a03cf505de661b69d59a837e0f63e9ab310a2db6
Nick Booker <NMBooker@gmail.com>
2015-01-17 16:08:01 +0000

    Warn of possible future deprecations.

--- funbox/passthrough.py
+++ funbox/passthrough.py
@@ -5,6 +5,17 @@
 Say you want to convert a list of strings to uppercase, but some entries might
 be None for whatever reason, but you want to keep those None values.
 
+If you're going to run a value through a chain of functions in turn,
+some of which can fail with a None value, the funbox.maybe module
+may be your thing.
+
+Note that funbox.maybe is much more powerful than this module, so I recommend
+you use that instead.
+
+WARNING: In the near future, I plan to move apply_if and pass_if into the
+funbox.maybe module, and deprecate passnone in favour of
+funbox.maybe.maybe_c().
+
 Here's a function that converts a string to uppercase:
 
 >>> from operator import methodcaller
@@ -21,15 +32,19 @@ Note that passnone is curried, so you can use map instead:
 >>> list(map(passnone(toUpper), input_strings))
 ['A', 'B', 'CDE', 'F', None, 'G', 'H', None]
 
-Or you could use imap from itertools.
 
-For more complex usage, consider using Option from fn.monad.
+For even more complex usage, consider using Option from fn.monad.
 """
 
 from .flogic import fnot
 
 def passnone(f, default=None):
     """passnone(f)(val) returns None if val is None, else f(val).
+
+    Unless you're setting your own 'default', I recommend you use
+    maybe_c() from funbox.maybe instead.
+
+    WARNING: I'm very likely to deprecate passnone soon.
     """
     def passnone_f(val):
         """Return f(val) if val is not None, else None
@@ -46,6 +61,7 @@ def apply_if(predicate, function):
     If predicate(val) is True or True-like, returns function(val)
     Otherwise returns val as-is.
 
+    WARNING: This is likely to move into funbox.maybe soon.
 
     >>> contains_a = lambda astr : 'a' in astr
     >>> to_upper = lambda astr : astr.upper()
@@ -64,6 +80,8 @@ def pass_if(predicate, function):
 
     If predicate(val) is False or False-like, returns function(val)
     Otherwise returns val as-is.
+
+    WARNING: This is likely to move into funbox.maybe soon.
 
     >>> contains_a = lambda astr : 'a' in astr
     >>> to_upper = lambda astr : astr.upper()
