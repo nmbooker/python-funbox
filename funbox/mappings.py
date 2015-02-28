04a1347060e66403a419e6d9de8bd97046e799de
Nick Booker <nmb@nickbooker.uk>
2015-02-28 17:58:04 +0000

    Add example intended usage.

--- funbox/mappings.py
+++ funbox/mappings.py
@@ -81,10 +81,22 @@ def updated_with(orig_dict, *new_values):
     New keys are added, and the values of existing keys will take the
     values in new_values.
 
+    You can specify more than one update dictionary.
+    They are applied in order, so the values of any keys in later dictionaries
+    will overwrite the earlier ones.
+
     >>> updated_with({1: 2, 2: 4, 3: 2}, {1: 'two', 4: 'four'})
     {1: 'two', 2: 4, 3: 2, 4: 'four'}
     >>> updated_with({1: 2}, {1: 'two', 2: 4}, {2: 'four'})
     {1: 'two', 2: 'four'}
+
+    Example patterns:
+       def something(self, overrides=None):
+           return updated_with({some defaults},
+                self.one_set if self.one_set else {},
+                self.another_set if self.another_set else {},
+                overrides if overrides else {},
+           )
     """
     newdict = dict(orig_dict)
     for vals in new_values:
