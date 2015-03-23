265f685e0323d0e0bae000b50c5291c6f75542b2
Nick Booker <nmb@nickbooker.uk>
2015-03-23 19:41:43 +0000

    Release v0.10

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "funbox",
-    version = "0.9",
+    version = "0.10",
     packages = find_packages(),
     author = "Nick Booker",
     description = "Functional Toolbox",
